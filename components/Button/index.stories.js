82d54cc9f44a4563e55089d6e8a57ecd36979424
miwashutaro0611 <miwashuntaro19245@gmail.com>
2020-04-06 02:36:41 +0900

    lint導入

--- components/Button/index.stories.js
+++ components/Button/index.stories.js
@@ -2,13 +2,13 @@ import './_index.scss'
 
 export default {
   title: 'Button',
-};
+}
 
 export const ButtonTem = () => {
-  const btn = document.createElement('button');
+  const btn = document.createElement('button')
   btn.classList.add('c-button')
-  btn.type = 'button';
-  btn.innerText = 'Hello Button';
-  btn.addEventListener('click', e => console.log(e));
-  return btn;
-};
+  btn.type = 'button'
+  btn.innerText = 'Hello Button'
+  btn.addEventListener('click', (e) => console.log(e))
+  return btn
+}
