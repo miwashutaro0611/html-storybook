f880b0c1e17ecd4ec6241fa544aa050b8cb0d6fb
miwashutaro0611 <miwashuntaro19245@gmail.com>
2020-04-06 02:03:46 +0900

    addon-knobs color追加　

--- components/card/index.js
+++ components/card/index.js
@@ -1,17 +1,23 @@
-import { text } from "@storybook/addon-knobs"
+import { text, color } from "@storybook/addon-knobs"
 
 export const Template = () => {
   const cardTitle = text('Title', 'タイトル')
   const cardText = text('Text', 'テキストテキスト')
+
+  const label = 'Color';
+  const defaultValue = '#ff00ff';
+
+  const cardColor = color(label, defaultValue)
+
   return `
     <div class='c-card'>
-      <p class='c-card__title'>${cardTitle}</p>
+      <p class='c-card__title' style='color: ${cardColor}'>${cardTitle}</p>
       <p class='c-card__text'>
         ${cardText}
       </p>
     </div>
     <div class='c-card c-card--large'>
-      <p class='c-card__title'>${cardTitle}</p>
+      <p class='c-card__title' style='color: ${cardColor}'>${cardTitle}</p>
       <p class='c-card__text'>
         ${cardText}
       </p>
